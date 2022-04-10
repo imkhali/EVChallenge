4c89f7efc7757410879ff314606bde295122bafe
Islam Khalil <imkhalil07@gmail.com>
2022-04-10 19:59:00 +1000

    fix

--- helpers.py
+++ helpers.py
@@ -1,10 +1,19 @@
 def maxLenSeqAboveNumber(arr, number):
-    """return the length of longest subarray whith consecutive elements above number"""
-    max,curr=0,0
+    """ return the length of longest subarray of 'arr' with consecutive elements above 'number' """
+    longest,curr=0,0
     for x in arr:
         if x > number:
             curr+=1
         else:
             curr=0
-        if curr>max:max=curr
-    return max
+        if curr>longest:longest=curr
+    return longest
+
+assert maxLenSeqAboveNumber([1, 2, 1, 3, 2], 4) == 0
+assert maxLenSeqAboveNumber([1, 2, 1, 4, 2], 4) == 0
+assert maxLenSeqAboveNumber([1], 1) == 0
+assert maxLenSeqAboveNumber([2], 1) == 1
+assert maxLenSeqAboveNumber([1, 2, 3], 1) == 2
+assert maxLenSeqAboveNumber([1, 2, 1, 3], 1) == 1
+assert maxLenSeqAboveNumber([1, 2, 1, 3], 1) == 1
+assert maxLenSeqAboveNumber([1, 2, 1, 3, 2], 1) == 2
